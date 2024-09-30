c831d4996a5bedcb96d583f429bb29289da21184
Mohammad K. Abdulkader <79748348+mkce-99@users.noreply.github.com>
2024-09-30 10:23:13 +0200

    added vehicle and interface for vehicle

---
+++ Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public abstract class Vehicle : IVehicle
+    {
+        public string RegistrationNumber { get; set; }
+        public string Brand {  get; set; }
+        public string Color { get; set; }
+
+        protected Vehicle(string RegistrationNumber, string Brand, string Color)
+        {
+            this.RegistrationNumber = RegistrationNumber;
+            this.Brand = Brand;
+            this.Color = Color;
+            
+        }
+    }
+}
