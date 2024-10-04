328a8a2488d1d76b89efd73ced3d6e5388f07bf1
Mohammad K. Abdulkader <79748348+mkce-99@users.noreply.github.com>
2024-10-04 10:09:55 +0200

    moved display from garage to garageHandler

--- Garage.cs
+++ Garage.cs
@@ -1,59 +1,73 @@
-﻿using System;
+﻿using Garage;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Garage
+public class Garage<T> : IEnumerable<T> where T : IVehicle
 {
-    public class Garage<T> : IEnumerable<T> where T : IVehicle
+    private T[] vehicles; // array to define garage size
+    private int capacity;  // set the garage size
+    private int count = 0; // to track vehicle amounts
+    private IUI ui;
+
+    public Garage(int capacity, IUI ui)
     {
-        private T[] vehicles; // array to define garage size
-        private int capacity;  // set the garage size
-        private int count = 0; // to track vehicle amounts
-        private IUI ui;
-
-        public Garage(int capacity, IUI ui)
-        {
-            this.capacity = capacity;
-            vehicles = new T[capacity];
-            this.ui = ui;
-        }
-
-        public int Count => count;
-        public int Capacity => capacity;
-
-        public bool AddVehicle(T vehicle) // this method is used to add vehicle and diplay a message
-        {
-            vehicles[count++] = vehicle;
-            ui.Message($"{vehicle.GetType().Name} added to the garage.");
-            return true; // returns true if vehicle successfully added 
-        }
-
-        public bool RemoveVehicle(string RegistrationNumber)
-        {
-            for (int c = 0; c < count; c++)
-            {
-                if (vehicles[c].RegistrationNumber.Equals(RegistrationNumber, StringComparison.Ordinal)) // to find and match regNumber.
-                {
-                    ui.Message($"{vehicles[c].GetType().Name} removed from the garage.");
-                    vehicles[c] = vehicles[--count];
-                    return true;// returns true if vehicle successfully removed 
-                }
-            }
-            ui.Message("Vehicle not found in the garage!");
-            return false;
-        }
-
-        public IEnumerator<T> GetEnumerator() // use a foreach loop to iterate over the stored vehicles
-        {
-            for (int c = 0; c < count; c++)
-            {
-                yield return vehicles[c];
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        this.capacity = capacity;
+        vehicles = new T[capacity];
+        this.ui = ui;
     }
+
+    public int Count => count;
+    public int Capacity => capacity;
+
+    public bool AddVehicle(T vehicle) // This method is used to add vehicle and display a message
+    {
+        if (count >= capacity) // Check if the garage is full
+        {
+            ui.Message("The garage is full! Cannot add more vehicles.");
+            return false; // Return false if the garage is full
+        }
+
+        vehicles[count++] = vehicle;
+        ui.Message($"{vehicle.GetType().Name} added to the garage.");
+        return true; // Return true if the vehicle is successfully added
+    }
+
+    public bool RemoveVehicle(string RegistrationNumber)
+    {
+        for (int c = 0; c < count; c++)
+        {
+            if (vehicles[c].RegistrationNumber.Equals(RegistrationNumber, StringComparison.Ordinal)) // To find and match regNumber.
+            {
+                ui.Message($"{vehicles[c].GetType().Name} removed from the garage.");
+                vehicles[c] = vehicles[--count]; // Decrement count and shift vehicles
+                return true; // Returns true if vehicle successfully removed 
+            }
+        }
+        ui.Message("Vehicle not found in the garage!");
+        return false;
+    }
+
+    public void DisplayVehicles() // Display all vehicles in the garage
+    {
+        if (count == 0)
+        {
+            ui.Message("No vehicles in the garage.");
+            return;
+        }
+
+        foreach (var vehicle in this) // Using IEnumerable to iterate over the stored vehicles
+        {
+            vehicle.DisplayInfo();
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator() // Use a foreach loop to iterate over the stored vehicles
+    {
+        for (int c = 0; c < count; c++) // Iterate only over added vehicles
+        {
+            yield return vehicles[c];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
--- GarageHandler.cs
+++ GarageHandler.cs
@@ -1,70 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Garage;
 
-namespace Garage
+public class GarageHandler : IHandler
 {
-    public class GarageHandler : IHandler
+    private Garage<IVehicle> garage;
+    private IUI ui;
+
+    public int GarageCount => garage.Count; // this property returns current number of vehicles in the garage
+
+    public GarageHandler(int capacity, IUI ui)
     {
-        private Garage<IVehicle> garage;
-        private IUI ui;
+        garage = new Garage<IVehicle>(capacity, ui);
+        this.ui = ui;
+    }
 
-        public int GarageCount => garage.Count; // this property returns current number of vehicles in the garage
+    public void Add(IVehicle vehicle) // this method ensures that the required properties is passed.
+    {
+        garage.AddVehicle(vehicle);
+    }
 
-        public GarageHandler(int capacity, IUI ui)
+    public void Remove(string registrationNumber) // this method is used to pass the registration number for removing vehicle from the garage
+    {
+        garage.RemoveVehicle(registrationNumber);
+    }
+
+    public void Display() // this method calls the DisplayVehicles method in the Garage class.
+    {
+        garage.DisplayVehicles();
+    }
+
+    public bool IsEmpty() // checks if the garage is empty
+    {
+        return garage.Count == 0;
+    }
+
+    public bool IsFull() // checks if the garage is full
+    {
+        return garage.Count >= garage.Capacity;
+    }
+
+    public IVehicle SearchByRegNumber(string regNumber) // this method is used to search by registration number.
+    {
+        var vehicle = garage.FirstOrDefault(v => v.RegistrationNumber.Equals(regNumber, StringComparison.Ordinal)); // FirstOrDefault is used to return first element that matchs.
+        if (vehicle != null)
         {
-            garage = new Garage<IVehicle>(capacity, ui);
-            this.ui = ui;
+            ui.Message(
+                $" - Vehicle found: {vehicle.GetType().Name}\n " +
+                $" - Registration Number: {vehicle.RegistrationNumber}\n " +
+                $" - Brand: {vehicle.Brand}\n" +
+                $" - Color: {vehicle.Color}");
         }
-
-        public void Add(IVehicle vehicle) // this method ensures that the required properties is passed.
+        else
         {
-            garage.AddVehicle(vehicle);
+            ui.Message("Vehicle not found");
         }
-
-        public void Remove(string registrationNumber) // this method is used to pass the registration number for removing vehicle from the garage
-        {
-            garage.RemoveVehicle(registrationNumber);
-        }
-
-        public void Display()
-        {
-            foreach (var vehicle in garage) // this method is used to loop through the stored vehicles.
-            {
-                vehicle.DisplayInfo();
-                //ui.Message($"Vehicle Type: {vehicle.GetType().Name} - Registration Number: {vehicle.RegistrationNumber} - Brand: {vehicle.Brand} - Color: {vehicle.Color}");
-            }
-        }
-
-        public bool IsEmpty() // checks if the garage is empty
-        {
-            return garage.Count == 0;
-
-        }
-
-        public bool IsFull() // checks if the garage is full
-        {
-            return garage.Count >= garage.Capacity;
-        }
-
-        public IVehicle SearchByRegNumber(string regNumber) // this method is used to search by registration number.
-        {
-            var vehicle = garage.FirstOrDefault(v => v.RegistrationNumber.Equals(regNumber, StringComparison.Ordinal)); // FirstOrDefault is used to return first element that matchs.
-            if (vehicle != null)
-            {
-                ui.Message(
-                    $" - Vehicle found: {vehicle.GetType().Name} " +
-                    $" - Registration Number: {vehicle.RegistrationNumber} " +
-                    $" - Brand: {vehicle.Brand}" +
-                    $" - Color: {vehicle.Color}");
-            }
-            else
-            {
-                ui.Message("Vehicle not found");
-            }
-            return vehicle!;
-        }
+        return vehicle!;
     }
 }
